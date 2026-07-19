8981665fa4c2e7e543ec5a9507879b35394c5dc5
ba-- <rikutobaba1009@gmail.com>
2026-07-19 15:09:26 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = "chord-ear-v1";
+const ASSETS = [
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  const url = new URL(e.request.url);
+  // Google Fonts など外部は素通し（キャッシュしない）
+  if (url.origin !== location.origin) return;
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request))
+  );
+});
